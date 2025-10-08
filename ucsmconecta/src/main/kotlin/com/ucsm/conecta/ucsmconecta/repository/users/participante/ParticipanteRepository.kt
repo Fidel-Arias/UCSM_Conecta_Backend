20ae1238580b9665e39286a26af6af5a2d213317
Fidel-Arias <fr06arias16@gmail.com>
2025-10-07 19:43:18 -0500

    Fix: Correcion en el repository de ParticipanteRepository.kt

--- ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/repository/users/participante/ParticipanteRepository.kt
+++ ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/repository/users/participante/ParticipanteRepository.kt
@@ -1,11 +1,19 @@
 package com.ucsm.conecta.ucsmconecta.repository.users.participante
 
 import com.ucsm.conecta.ucsmconecta.domain.users.participante.Participante
+import com.ucsm.conecta.ucsmconecta.dto.users.profile.participante.ParticipanteBusquedaDTO
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.util.Optional
 
 interface ParticipanteRepository : JpaRepository<Participante, Long> {
     fun findByNumDocumento(numDocumento: String): Optional<Participante>
-    fun findByName(nombre: String): List<Participante>
-    fun findByApellidos(apPaterno: String, apMaterno: String): Optional<Participante>
+    fun findByNombres(nombres: String): List<Participante>
+
+    @Query(
+        value = "SELECT * FROM buscar_participante_apellidos(:busqueda)",
+        nativeQuery = true
+    )
+    fun findByApellidos(@Param("busqueda") busqueda: String): List<ParticipanteBusquedaDTO>
 }
