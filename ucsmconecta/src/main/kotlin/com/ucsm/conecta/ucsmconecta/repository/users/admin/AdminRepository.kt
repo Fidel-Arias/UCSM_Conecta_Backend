64296a9c8834635b2917b9d34367b9a12c0155c5
Fidel-Arias <fr06arias16@gmail.com>
2025-10-09 03:22:40 -0500

    Rebuild: Aplicacion de la funcion creada dentro de la base de datos para la busqueda de administradores

--- ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/repository/users/admin/AdminRepository.kt
+++ ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/repository/users/admin/AdminRepository.kt
@@ -2,8 +2,13 @@ package com.ucsm.conecta.ucsmconecta.repository.users.admin
 
 import com.ucsm.conecta.ucsmconecta.domain.users.administrador.Administrador
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface AdminRepository: JpaRepository<Administrador, Long> {
-    fun findByNombres(nombres: String): List<Administrador>
+    @Query(
+        value = "SELECT * FROM buscar_admin_nombres(:nombres)",
+        nativeQuery = true)
+    fun findByNombres(@Param("nombres") nombres: String): List<Administrador>
     fun findByEscuelaProfesionalId(escuelaProfesionalId: Long): List<Administrador>
 }
