833e31bfaade81878269b1e7ac775e333209237b
Fidel-Arias <fr06arias16@gmail.com>
2025-10-21 03:12:00 -0500

    Refactor: EscuelaProfesionalService to use EntityNotFoundException for error handling and add method to retrieve all EscuelaProfesionales

--- ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/services/universidad/carrera/EscuelaProfesionalService.kt
+++ ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/services/universidad/carrera/EscuelaProfesionalService.kt
@@ -3,20 +3,22 @@ package com.ucsm.conecta.ucsmconecta.services.universidad.carrera
 import com.ucsm.conecta.ucsmconecta.domain.universidad.carrera.EscuelaProfesional
 import com.ucsm.conecta.ucsmconecta.dto.universidad.carrera.DataRequestEscuelaProfesional
 import com.ucsm.conecta.ucsmconecta.repository.universidad.carrera.EscuelaProfesionalRepository
+import jakarta.persistence.EntityNotFoundException
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.RequestBody
+import java.util.Optional
 
 @Service
 class EscuelaProfesionalService @Autowired constructor(
     private val escuelaProfesionalRepository: EscuelaProfesionalRepository
 ){
     fun searchById(id: Long): EscuelaProfesional = escuelaProfesionalRepository.findById(id)
-        .orElseThrow { RuntimeException("Escuela Profesional no encontrada") }
+        .orElseThrow { EntityNotFoundException("Escuela Profesional no encontrada") }
 
     fun searchByNombre(nombre: String): EscuelaProfesional = escuelaProfesionalRepository.findByNombre(nombre)
-        .orElseThrow { RuntimeException("Escuela Profesional no encontrada") }
+        .orElseThrow { EntityNotFoundException("Escuela Profesional no encontrada") }
 
     fun createEscuelaProfesional(@RequestBody @Valid dataRequestEscuelaProfesional: DataRequestEscuelaProfesional): EscuelaProfesional {
         // Lógica para crear una escuela profesional
@@ -24,4 +26,6 @@ class EscuelaProfesionalService @Autowired constructor(
             nombre = dataRequestEscuelaProfesional.nombre,
         ))
     }
+
+    fun getAllEscuelasProfesionales(): List<EscuelaProfesional> = escuelaProfesionalRepository.findAll()
 }
