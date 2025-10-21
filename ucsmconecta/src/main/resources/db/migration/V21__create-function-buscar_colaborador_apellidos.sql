b03b367fd707196415d56e67f1faf9e586df73b0
Fidel-Arias <fr06arias16@gmail.com>
2025-10-21 02:51:44 -0500

    Build: Creacion de una funcion en mysql para buscar colaborador por apellidos.

---
+++ ucsmconecta/src/main/resources/db/migration/V21__create-function-buscar_colaborador_apellidos.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION buscar_colaborador_apellidos(p_busqueda TEXT)
+RETURNS TABLE(
+    nombres VARCHAR,
+    a_paterno VARCHAR,
+    a_materno VARCHAR,
+    estado BOOL,
+)
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        c.nombres,
+        c.a_paterno,
+        c.a_materno,
+        c.estado
+    FROM colaborador c
+    WHERE
+        -- Buscar por apellido paterno
+        LOWER(c.a_paterno) LIKE LOWER(CONCAT('%', p_busqueda, '%'))
+        OR
+        -- Buscar por apellidos concatenados (ej: "Gomez Perez")
+        LOWER(CONCAT(c.a_paterno, ' ', c.a_materno)) LIKE LOWER(CONCAT('%', p_busqueda, '%'));
+END;
+$$ LANGUAGE plpgsql;
