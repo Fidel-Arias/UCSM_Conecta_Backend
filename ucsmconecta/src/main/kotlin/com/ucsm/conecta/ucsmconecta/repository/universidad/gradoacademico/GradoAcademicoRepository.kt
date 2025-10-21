5dd780f4f554a704df8a0de015b2bcbf20f68b58
Fidel-Arias <fr06arias16@gmail.com>
2025-10-21 03:01:16 -0500

    Build: Creacion del repositorio GradoAcademico para gestionar entidades de grado académico

---
+++ ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/repository/universidad/gradoacademico/GradoAcademicoRepository.kt
@@ -0,0 +1,9 @@
+package com.ucsm.conecta.ucsmconecta.repository.universidad.gradoacademico
+
+import com.ucsm.conecta.ucsmconecta.domain.universidad.gradoacademico.GradoAcademico
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
+
+interface GradoAcademicoRepository: JpaRepository<GradoAcademico, Long> {
+    fun findByDescripcion(descripcion: String): Optional<GradoAcademico>
+}
