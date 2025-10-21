269dcca1bfbb6d981cec1ca7bb540beb2f760dee
Fidel-Arias <fr06arias16@gmail.com>
2025-10-21 05:24:13 -0500

    Refactor: Add createTipoParticipante method and update searchById to handle EntityNotFoundException

--- ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/services/users/TipoParticipanteService.kt
+++ ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/services/users/TipoParticipanteService.kt
@@ -3,8 +3,10 @@ package com.ucsm.conecta.ucsmconecta.services.users
 import com.ucsm.conecta.ucsmconecta.domain.users.participante.TipoParticipante
 import com.ucsm.conecta.ucsmconecta.repository.users.participante.TipoParticipanteRepository
 import jakarta.persistence.EntityNotFoundException
+import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.RequestBody
 
 @Service
 class TipoParticipanteService @Autowired constructor(
@@ -15,4 +17,14 @@ class TipoParticipanteService @Autowired constructor(
 
     fun searchById(id: Long): TipoParticipante? = tipoParticipanteRepository.findById(id)
         .orElseThrow { EntityNotFoundException("Tipo de Participante no encontrado") }
+
+    fun createTipoParticipante(@RequestBody @Valid descripcion: String): TipoParticipante? {
+        val nuevoTipoParticipante = TipoParticipante(
+            descripcion = descripcion
+        )
+
+        return tipoParticipanteRepository.save(nuevoTipoParticipante)
+    }
+
+    fun getAllTiposParticipantes(): List<TipoParticipante> = tipoParticipanteRepository.findAll()
 }
