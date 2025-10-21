a360046b6f431eca7d3003949db126fbebc8b858
Fidel-Arias <fr06arias16@gmail.com>
2025-10-21 03:51:46 -0500

    Refactor: Update CongresoService to use EntityNotFoundException for error handling in search methods

--- ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/services/universidad/congresos/CongresoService.kt
+++ ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/services/universidad/congresos/CongresoService.kt
@@ -2,6 +2,7 @@ package com.ucsm.conecta.ucsmconecta.services.universidad.congresos
 
 import com.ucsm.conecta.ucsmconecta.domain.universidad.congresos.Congreso
 import com.ucsm.conecta.ucsmconecta.repository.universidad.congresos.CongresoRepository
+import jakarta.persistence.EntityNotFoundException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -9,9 +10,9 @@ import org.springframework.stereotype.Service
 class CongresoService @Autowired constructor(
     private val congresoRepository: CongresoRepository
 ){
-    fun searchById(id: Long):Congreso = congresoRepository.findById(id)
-        .orElseThrow { RuntimeException("Congreso no encontrado") }
+    fun searchById(id: Long):Congreso? = congresoRepository.findById(id)
+        .orElseThrow { EntityNotFoundException("Congreso no encontrado") }
 
-    fun searchByNombre(nombre: String):Congreso = congresoRepository.findByNombre(nombre)
-        .orElseThrow { RuntimeException("Congreso no encontrado") }
+    fun searchByNombre(nombre: String):Congreso? = congresoRepository.findByNombre(nombre)
+        .orElseThrow { EntityNotFoundException("Congreso no encontrado") }
 }
