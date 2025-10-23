cc9e1b97b02f4b9261408c81f4023d1aa9c58bb3
Fidel-Arias <fr06arias16@gmail.com>
2025-10-23 06:41:07 -0500

    feat: Enhance ParticipanteController with additional search endpoints and integrate EscuelaProfesional and TipoParticipante services

--- ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/controller/users/participante/ParticipanteController.kt
+++ ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/controller/users/participante/ParticipanteController.kt
@@ -1,9 +1,14 @@
 package com.ucsm.conecta.ucsmconecta.controller.users.participante
 
+import com.ucsm.conecta.ucsmconecta.domain.universidad.carrera.EscuelaProfesional
 import com.ucsm.conecta.ucsmconecta.domain.users.participante.Participante
+import com.ucsm.conecta.ucsmconecta.domain.users.participante.TipoParticipante
 import com.ucsm.conecta.ucsmconecta.dto.users.auth.participante.RegisterParticipanteData
 import com.ucsm.conecta.ucsmconecta.dto.users.profile.participante.DataResponseParticipante
+import com.ucsm.conecta.ucsmconecta.dto.users.profile.participante.ParticipanteBusquedaDTO
+import com.ucsm.conecta.ucsmconecta.services.universidad.carrera.EscuelaProfesionalService
 import com.ucsm.conecta.ucsmconecta.services.users.ParticipanteService
+import com.ucsm.conecta.ucsmconecta.services.users.TipoParticipanteService
 import jakarta.transaction.Transactional
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
@@ -19,14 +24,29 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 @RestController
 @RequestMapping("/api/participantes")
 class ParticipanteController @Autowired constructor(
-    private val participanteService: ParticipanteService
-) {
+    private val participanteService: ParticipanteService,
+    private val escuelaProfesionalService: EscuelaProfesionalService,
+    private val tipoParticipanteService: TipoParticipanteService
+    ) {
 
     @PostMapping
     @Transactional
-    fun createParticipante(@RequestBody @Valid registerParticipanteData: RegisterParticipanteData, uriComponentsBuilder: ServletUriComponentsBuilder): ResponseEntity<DataResponseParticipante> {
+    fun createParticipante(
+        @RequestBody @Valid registerParticipanteData: RegisterParticipanteData,
+        uriComponentsBuilder: ServletUriComponentsBuilder
+    ): ResponseEntity<DataResponseParticipante> {
         // Crear el participante
         val participante: Participante? = participanteService.createParticipante(registerParticipanteData)
+
+        // Buscar la escuela profesional asociada
+        val escuelaProfesional: EscuelaProfesional? =
+            escuelaProfesionalService.getEscuelaProfesionalById(registerParticipanteData.escuelaProfesionalId)
+
+        // Buscar el tipo de participante asociado
+        val tipoParticipante = tipoParticipanteService.searchById(registerParticipanteData.tipoParticipanteId)
+
+        // Buscar el congreso asociado
+        val congreso = registerParticipanteData.congresoId
 
         // Se pasan los datos creados a DataResponseParticipante para visualizarlos
         val dataResponseParticipante = DataResponseParticipante(
@@ -36,8 +56,9 @@ class ParticipanteController @Autowired constructor(
             apMaterno = participante.apMaterno,
             estado = participante.estado,
             nDocumento = participante.numDocumento,
-            escuelaProfesionalId = participante.escuelaProfesional?.id,
-            tipoParticipanteId = participante.tipoParticipante?.id,
+            escuelaProfesionalId = escuelaProfesional?.id,
+            tipoParticipanteId = tipoParticipante?.id,
+            congresoId = congreso,
             qrCode = participante.qr_code
         )
 
@@ -51,7 +72,19 @@ class ParticipanteController @Autowired constructor(
 
     @GetMapping("/{id}")
     fun getParticipanteById(@PathVariable id: Long): ResponseEntity<DataResponseParticipante> {
+        // Buscar el participante por ID
         val participante = participanteService.getParticipanteById(id)
+
+        // Buscar la escuela profesional asociada
+        val escuelaProfesional: EscuelaProfesional? =
+            escuelaProfesionalService.getEscuelaProfesionalById(participante?.escuelaProfesional?.id!!)
+
+        // Buscar el tipo de participante asociado
+        val tipoParticipante: TipoParticipante? =
+            tipoParticipanteService.searchById(participante.tipoParticipante?.id!!)
+
+        // Buscar el congreso asociado
+        val congreso = participante?.congreso?.id
 
         val dataResponseParticipante = DataResponseParticipante(
             id = participante?.id!!,
@@ -60,8 +93,9 @@ class ParticipanteController @Autowired constructor(
             apMaterno = participante.apMaterno,
             estado = participante.estado,
             nDocumento = participante.numDocumento,
-            escuelaProfesionalId = participante.escuelaProfesional?.id,
-            tipoParticipanteId = participante.tipoParticipante?.id,
+            escuelaProfesionalId = escuelaProfesional?.id,
+            tipoParticipanteId = tipoParticipante?.id,
+            congresoId = congreso,
             qrCode = participante.qr_code
         )
 
@@ -86,10 +120,65 @@ class ParticipanteController @Autowired constructor(
                 nDocumento = participante.numDocumento,
                 escuelaProfesionalId = participante.escuelaProfesional?.id,
                 tipoParticipanteId = participante.tipoParticipante?.id,
+                congresoId = participante.congreso?.id,
                 qrCode = participante.qr_code
             )
         }
 
+        return ResponseEntity.ok(dataResponseParticipantes)
+    }
+
+    @GetMapping("/search/apellidos/{apPaterno}/{apMaterno}")
+    fun searchParticipanteByApellidos(
+        @PathVariable apPaterno: String,
+        @PathVariable apMaterno: String
+    ): ResponseEntity<List<ParticipanteBusquedaDTO>> {
+        val participantes = participanteService.searchByApellidos(apPaterno, apMaterno)
+
+        val dataResponseParticipantes: List<ParticipanteBusquedaDTO> = participantes.map { participante ->
+            ParticipanteBusquedaDTO(
+                nombres = participante.nombres,
+                apPaterno = participante.apPaterno,
+                apMaterno = participante.apMaterno,
+                estado = participante.estado,
+                numDocumento = participante.numDocumento,
+            )
+        }
+        return ResponseEntity.ok(dataResponseParticipantes)
+    }
+
+    @GetMapping("/search/documento/{numDocumento}")
+    fun searchParticipanteByNumDocumento(@PathVariable numDocumento: String): ResponseEntity<ParticipanteBusquedaDTO> {
+        // Buscar el participante por número de documento
+        val participante: Participante? = participanteService.searchByNumDocumento(numDocumento)
+
+        // Mapear a ParticipanteBusquedaDTO
+        val dataResponseParticipante: ParticipanteBusquedaDTO = ParticipanteBusquedaDTO(
+            nombres = participante?.nombres!!,
+            apPaterno = participante.apPaterno,
+            apMaterno = participante.apMaterno,
+            estado = participante.estado,
+            numDocumento = participante.numDocumento,
+        )
+
+        return ResponseEntity.ok(dataResponseParticipante)
+    }
+
+    @GetMapping("/search/nombres/{nombres}")
+    fun searchParticipanteByNombres(@PathVariable nombres: String): ResponseEntity<List<ParticipanteBusquedaDTO>> {
+        // Buscar participantes por nombres
+        val participantes: List<Participante> = participanteService.searchByNombres(nombres)
+
+        // Mapear a lista de ParticipanteBusquedaDTO
+        val dataResponseParticipantes: List<ParticipanteBusquedaDTO> = participantes.map { participante ->
+            ParticipanteBusquedaDTO(
+                nombres = participante.nombres,
+                apPaterno = participante.apPaterno,
+                apMaterno = participante.apMaterno,
+                estado = participante.estado,
+                numDocumento = participante.numDocumento,
+            )
+        }
         return ResponseEntity.ok(dataResponseParticipantes)
     }
 }
