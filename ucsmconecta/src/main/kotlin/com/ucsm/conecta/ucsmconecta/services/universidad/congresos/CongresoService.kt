2445daa40ea6b37a2b7f234904902e8911403f44
Fidel-Arias <fr06arias16@gmail.com>
2025-10-23 06:38:23 -0500

    Refactor: Enhance CongresoService to include createCongreso method and improve error handling

--- ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/services/universidad/congresos/CongresoService.kt
+++ ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/services/universidad/congresos/CongresoService.kt
@@ -1,18 +1,41 @@
 package com.ucsm.conecta.ucsmconecta.services.universidad.congresos
 
+import com.ucsm.conecta.ucsmconecta.domain.universidad.carrera.EscuelaProfesional
 import com.ucsm.conecta.ucsmconecta.domain.universidad.congresos.Congreso
+import com.ucsm.conecta.ucsmconecta.dto.universidad.congresos.DataRequestCongreso
 import com.ucsm.conecta.ucsmconecta.repository.universidad.congresos.CongresoRepository
+import com.ucsm.conecta.ucsmconecta.services.universidad.carrera.EscuelaProfesionalService
 import jakarta.persistence.EntityNotFoundException
+import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.RequestBody
 
 @Service
 class CongresoService @Autowired constructor(
-    private val congresoRepository: CongresoRepository
+    private val congresoRepository: CongresoRepository,
+    private val escuelaProfesionalService: EscuelaProfesionalService
 ){
-    fun searchById(id: Long):Congreso? = congresoRepository.findById(id)
+    fun getCongresoById(id: Long):Congreso? = congresoRepository.findById(id)
         .orElseThrow { EntityNotFoundException("Congreso no encontrado") }
 
     fun searchByNombre(nombre: String):Congreso? = congresoRepository.findByNombre(nombre)
         .orElseThrow { EntityNotFoundException("Congreso no encontrado") }
+
+    fun createCongreso(@RequestBody @Valid dataRequestCongreso: DataRequestCongreso): Congreso {
+        val escuelaProfesional: EscuelaProfesional? = escuelaProfesionalService.getEscuelaProfesionalById(
+            dataRequestCongreso.escuelaProfesionalId
+        )
+
+        return congresoRepository.save(
+            Congreso(
+                nombre = dataRequestCongreso.nombre,
+                fechaInicio = dataRequestCongreso.fechaInicio,
+                fechaFin = dataRequestCongreso.fechaFin,
+                numAsistencias = dataRequestCongreso.numAsistencias,
+                numRefrigerios = dataRequestCongreso.numRefrigerios,
+                escuelaProfesional = escuelaProfesional!!
+            )
+        )
+    }
 }
