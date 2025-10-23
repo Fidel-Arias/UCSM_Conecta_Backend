fd7b1f0948902fb5122ee93ff82969a9b02fd6d3
Fidel-Arias <fr06arias16@gmail.com>
2025-10-23 06:36:34 -0500

    Refactor: Create ColaboradorRepository to add search functionalities by apellidos and nombres

---
+++ ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/repository/users/colaborador/ColaboradorRepository.kt
@@ -0,0 +1,19 @@
+package com.ucsm.conecta.ucsmconecta.repository.users.colaborador
+
+import com.ucsm.conecta.ucsmconecta.domain.users.colaborador.Colaborador
+import com.ucsm.conecta.ucsmconecta.dto.users.profile.colaborador.ColaboradorBusquedaDTO
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+
+interface ColaboradorRepository: JpaRepository<Colaborador, Long> {
+    @Query(
+        value = "SELECT * FROM buscar_colaborador_apellidos(:busqueda)",
+        nativeQuery = true)
+    fun findByApellidos(@Param("busqueda") busqueda: String): List<ColaboradorBusquedaDTO>
+
+    @Query(
+        value = "SELECT * FROM buscar_colaborador_nombres(:busqueda)",
+        nativeQuery = true)
+    fun findByNombres(@Param("busqueda") busqueda: String): List<ColaboradorBusquedaDTO>
+}
