8fc4fba7216d3b536d4f93cff5160340df6ba7a5
Fidel-Arias <fr06arias16@gmail.com>
2025-10-24 22:37:28 -0500

    refactor: Remove unused findByNombres method from AdminRepository

--- ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/repository/users/admin/AdminRepository.kt
+++ ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/repository/users/admin/AdminRepository.kt
@@ -6,9 +6,5 @@ import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
 interface AdminRepository: JpaRepository<Administrador, Long> {
-    @Query(
-        value = "SELECT * FROM buscar_admin_nombres(:nombres)",
-        nativeQuery = true)
-    fun findByNombres(@Param("nombres") nombres: String): List<Administrador>
-    fun findByEscuelaProfesionalId(escuelaProfesionalId: Long): List<Administrador>
+
 }
