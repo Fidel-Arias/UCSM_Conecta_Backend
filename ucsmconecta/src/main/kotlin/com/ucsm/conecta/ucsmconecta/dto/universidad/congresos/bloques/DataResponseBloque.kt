9c77e5f525b0d539ae573b73ef0ea922d55ac24f
Fidel-Arias <fr06arias16@gmail.com>
2025-10-25 07:23:35 -0500

    Refactor: Modificacion de los tipos de datos en DataResponseBloque

--- ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/dto/universidad/congresos/bloques/DataResponseBloque.kt
+++ ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/dto/universidad/congresos/bloques/DataResponseBloque.kt
@@ -1,10 +1,15 @@
 package com.ucsm.conecta.ucsmconecta.dto.universidad.congresos.bloques
 
+import com.ucsm.conecta.ucsmconecta.dto.universidad.congresos.dia.DataResultDia
+import com.ucsm.conecta.ucsmconecta.dto.universidad.congresos.ponencias.DataResultPonencia
+import com.ucsm.conecta.ucsmconecta.dto.universidad.congresos.ubicacion.DataResultUbicacion
+import java.time.LocalTime
+
 data class DataResponseBloque(
     val id: Long,
-    val horaInicial: String,
-    val horaFinal: String,
-    val diaId: Long,
-    val ubicacionId: Long,
-    val ponenciaId: Long
+    val horaInicial: LocalTime,
+    val horaFinal: LocalTime,
+    val dia: DataResultDia,
+    val ubicacion: DataResultUbicacion,
+    val ponencia: DataResultPonencia
 )
