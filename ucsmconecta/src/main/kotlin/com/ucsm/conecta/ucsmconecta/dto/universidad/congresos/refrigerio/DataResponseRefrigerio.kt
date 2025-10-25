d6def36e3d53e65b01b6be11e80e1d3f2ac690ca
Fidel-Arias <fr06arias16@gmail.com>
2025-10-25 08:57:54 -0500

    Refactor: Cambio en los tipos de datos

--- ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/dto/universidad/congresos/refrigerio/DataResponseRefrigerio.kt
+++ ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/dto/universidad/congresos/refrigerio/DataResponseRefrigerio.kt
@@ -1,11 +1,13 @@
 package com.ucsm.conecta.ucsmconecta.dto.universidad.congresos.refrigerio
 
+import com.ucsm.conecta.ucsmconecta.dto.universidad.congresos.DataResultCongreso
+import com.ucsm.conecta.ucsmconecta.dto.users.profile.participante.DataResultParticipante
 import java.time.LocalDate
 
 data class DataResponseRefrigerio(
     val id: Long,
     val fecha: LocalDate,
     val estado: Boolean,
-    val participanteId: Long,
-    val congresoId: Long
+    val participante: DataResultParticipante,
+    val congreso: DataResultCongreso
 ){}
