81650d3c5e7319a41a06febbd4e69d1dab87c45b
Fidel-Arias <fr06arias16@gmail.com>
2025-10-25 09:01:31 -0500

    Feat: Creacion de un data class corta para Participante

---
+++ ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/dto/users/profile/participante/DataResultParticipante.kt
@@ -0,0 +1,8 @@
+package com.ucsm.conecta.ucsmconecta.dto.users.profile.participante
+
+data class DataResultParticipante(
+    val nombres: String,
+    val apPaterno: String,
+    val apMaterno: String,
+    val numDocumento: String,
+)
