9a0ee7daf66e7c2b84e6da278c8679723339aa5d
Fidel-Arias <fr06arias16@gmail.com>
2025-11-08 07:49:05 -0500

    Refactor: Cambio en el tipo de dato de estado, puede ser null

--- ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/dto/universidad/congresos/ubicacion/DataResponseUbicacion.kt
+++ ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/dto/universidad/congresos/ubicacion/DataResponseUbicacion.kt
@@ -3,6 +3,6 @@ package com.ucsm.conecta.ucsmconecta.dto.universidad.congresos.ubicacion
 data class DataResponseUbicacion(
     val id: Long,
     val nombre: String,
-    val estado: Boolean,
+    val estado: Boolean?,
 ) {
 }
