cb274e6cb3c5ba11457cd0f7dd0ee0a19fc76ad5
Fidel-Arias <fr06arias16@gmail.com>
2025-11-09 01:14:08 -0500

    Feat: Dto sin congreso

---
+++ ucsmconecta/src/main/kotlin/com/ucsm/conecta/ucsmconecta/dto/universidad/congresos/dia/DataResultDiaAsistencia.kt
@@ -0,0 +1,8 @@
+package com.ucsm.conecta.ucsmconecta.dto.universidad.congresos.dia
+
+import java.time.LocalDate
+
+data class DataResultDiaAsistencia(
+    val id: Long,
+    val fecha: LocalDate,
+)
